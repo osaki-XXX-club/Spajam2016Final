de5ab56d75e764358f3ee6752edfa8c803f3ab69
itaru ogawa <i.ogawa.z8@future.co.jp>
2016-07-03 05:25:45 +0900

    初期画面を追加

---
+++ app/src/main/java/jp/co/future/androidbase/activity/InitialActivity.java
@@ -0,0 +1,63 @@
+package jp.co.future.androidbase.activity;
+
+/**
+ * Created by itaru on 2016/07/03.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+
+import java.util.ArrayList;
+
+import jp.co.future.androidbase.R;
+// other imports and package statement are omitted
+
+public class InitialActivity extends AppCompatActivity
+        implements ObservableScrollViewCallbacks {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+
+        ObservableListView listView = (ObservableListView) findViewById(R.id.list);
+        listView.setScrollViewCallbacks(this);
+
+        // TODO These are dummy. Populate your data here.
+        ArrayList<String> items = new ArrayList<String>();
+        for (int i = 1; i <= 100; i++) {
+            items.add("Item " + i);
+        }
+        listView.setAdapter(new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, items));
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll,
+                                boolean dragging) {
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+        ActionBar ab = getSupportActionBar();
+        if (scrollState == ScrollState.UP) {
+            if (ab.isShowing()) {
+                ab.hide();
+            }
+        } else if (scrollState == ScrollState.DOWN) {
+            if (!ab.isShowing()) {
+                ab.show();
+            }
+        }
+    }
+}
